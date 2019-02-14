9c5c8da4db49308dfc679c6f46383e7de254accb
Mateus Landim <mateus.landim@lsbd.ufc.br>
2019-02-14 17:12:49 -0300

    update

--- client2.py
+++ client2.py
@@ -8,17 +8,34 @@ print("libzmq-" + zmq.zmq_version())
 if zmq.zmq_version_info() < (4, 0):
     raise RuntimeError("version not supported")
 
+#Init Global Variables
+socket = None
+monitor = None
+event = None
+
 def event_monitor():
     global monitor
     global socket
     global event
-    event = evt['event']
+    evt = {'event': 0}
+    print(3)
+    monitor = socket.get_monitor_socket()
+    try:
+        evt = recv_monitor_message(monitor, flags=zmq.NOBLOCK)
+        event = evt['event']
+    except Exception as e:
+        print('Error:' + str(type(e)) + str(e))
+    print(6)
     print("Event: {}".format(evt['event']))
+    print(7)
     if evt['event'] == 512:
-        print('Client Disconected...\nTrying to Reconnect:\n')
-        for k in range(3):
+        print('Client Disconected...\n')
+        for k in range(10):
             socket.close()
+            print('Trying to Reconnect:...{}\n'.format(str(k)))
             connect()
+            evt = recv_monitor_message(monitor)
+            event = evt['event']
             time.sleep(2)
             if evt['event'] == 1 :
                 print('Reconnected...\n')
@@ -27,43 +44,40 @@ def event_monitor():
         if evt['event'] != 1 :
             print('Could NOT reconnect sucessfuly...\nExiting...')
             exit()
+    time.sleep(1)
 
 
 def connect():
-    global ctx
     global socket
-    global monitor
-    global evt
-    print('Trying connect to Server:\n...')
     try:
         ctx = zmq.Context.instance() 
         socket = ctx.socket(zmq.REQ)
-        socket.connect('tcp://127.0.0.1:5555') 
-        monitor = socket.get_monitor_socket()
-        evt = recv_monitor_message(monitor)
+        socket.connect('tcp://127.0.0.1:3000') 
     except Exception as e :
         print('Error:' + str(type(e)) + str(e))
 
 def main():
-
+    global event
+    global socket
+    message = None
     while True:
+        print(1)
+        event_monitor()
+        print(2)
         if event == 1:
             print('Sending request ...')
-            socket.send_string('Hello')
+            try:
+                socket.send_string('Hello', flags=zmq.NOBLOCK, copy=False, track=False)
+                message = socket.recv(flags=zmq.NOBLOCK)
+            except Exception as e :
+                if message:
+                    print('Error:' + str(type(e)) + str(e))
             #Get the answer
-            message = str(socket.recv())
-            print('Received from server : ' + message)
+            print('Received from server : ' + str(message))
         time.sleep(1)
 
 if __name__ == "__main__":
-    #Init Global Variables
-    ctx = None
-    socket = None
-    monitor = None
-    event = None
-    evt = None
     #Start Conection
     connect()
-    t = threading.Thread(target=event_monitor, args=())
-    t.start()
+    print('Trying connect to server...\n')
     main()
